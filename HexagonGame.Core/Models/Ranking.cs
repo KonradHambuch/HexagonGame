b94b0c11c494a1487e16371046ad163234e9ed0e
kiscsavo3 <tremmel.marton@gmail.com>
2021-11-18 19:32:18 +0100

    database

---
+++ HexagonGame.Core/Models/Ranking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexagonGame.Core.Models
+{
+    public class Ranking
+    {
+        public int Score { get; set; }
+    }
+}
--- HexagonGame/Services/Database.cs
+++ HexagonGame/Services/Database.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using HexagonGame.Core.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Windows.Storage;
 
 namespace HexagonGame.Services
 {
-    class Database
+    public class Database
     {
+        public Dictionary<string, Ranking> Rankings { get; set; } = new Dictionary<string, Ranking>();
+        public string DbFilePath { get; set; }
+        public Database()
+        {
+            var appFolder = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            var work_dir = appFolder + @"\Db_files\";
+            DbFilePath = work_dir + "db_file.json";
+            if (!Directory.GetFiles(appFolder).Contains(@"\Db_files\"))
+            {
+                Directory.CreateDirectory(work_dir);
+            }
+            var files = Directory.GetFiles(work_dir);
+
+            if (files.Contains(DbFilePath))
+            {
+                LoadRankings();
+            }
+        }
+        ~Database()
+        {
+            CommitRankings();
+        }
+
+        public void AddWinner(Player player)
+        {
+            if (Rankings.ContainsKey(player.Name))
+            {
+                Rankings[player.Name].Score++;
+            }
+            else
+            {
+                Rankings.Add(player.Name, new Ranking { Score = 1 });
+            }
+
+        }
+
+        public void LoadRankings()
+        {
+            using (StreamReader r = new StreamReader(DbFilePath))
+            {
+                string json = r.ReadToEnd();
+                Rankings = JsonConvert.DeserializeObject<Dictionary<string, Ranking>>(json);
+            }
+        }
+
+        public void CommitRankings()
+        {
+            var json = JsonConvert.SerializeObject(Rankings);
+
+            using (StreamWriter sw = new StreamWriter(DbFilePath))
+            {
+                sw.WriteLine(json);
+            }
+        }
     }
 }
